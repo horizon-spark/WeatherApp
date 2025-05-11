1f91787c08fa4626ead3839a9468b93d8dfb15ac
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-05-11 12:39:13 +0300

    Создана минимальная рабочая версия программы

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", getWeather);
-
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const weatherDescription = document.querySelector(".weather-description");
 const city = document.querySelector(".city");
+
+document.addEventListener("DOMContentLoaded", getWeather);
 city.addEventListener("keypress", setCity);
 
 async function getWeather() {
-    
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=9391ddcd941d0c7ac4b62518140148c8&units=metric`;
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=9391ddcd941d0c7ac4b62518140148c8&units=metric`;
 
     const res = await fetch(url);
     const data = await res.json();
-
-    const weatherIcon = document.querySelector(".weather-icon");
-    const temperature = document.querySelector(".temperature");
-    const weatherDescription = document.querySelector(".weather-decription");
     
     weatherIcon.className = "weather-icon owf";
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
