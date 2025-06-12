36090582a02538c698f97c7d77b2d11fa2fe769e
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-06-12 12:30:21 +0300

    Реализовано добавление карточки с погодой, необходимо реализовать добавление иконки, соответствующей погоде в выбранном городе

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
 "use strict";
 
-const weatherIcon = document.querySelector(".weather-icon");
-const temperature = document.querySelector(".temperature");
-const weatherDescription = document.querySelector(".weather-description");
-const city = document.querySelector(".city");
+const apiKey = "9391ddcd941d0c7ac4b62518140148c8";
 
-document.addEventListener("DOMContentLoaded", getWeather);
-city.addEventListener("keypress", setCity);
+const header = document.querySelector(".header");
+const form = document.querySelector("#form");
+const input = document.querySelector("#inputCity");
 
-async function getWeather() {
+form.onsubmit = async function(event) {
+    // Отменяем отправку формы
+    event.preventDefault();
 
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=9391ddcd941d0c7ac4b62518140148c8&units=metric`;
+    // Берем значение из инпута, обрезая пробельные символы
+    let city = input.value.trim();
 
-    const res = await fetch(url);
-    const data = await res.json();
-    
-    weatherIcon.className = "weather-icon owf";
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp}C`;
-    weatherDescription.textContent = data.weather[0].description;
-}
+    // Делаем запрос на сервер
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${apiKey}&units=metric`
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
 
-function setCity(event) {
-    if (event.code === "Enter") {
-        getWeather();
-        city.blur();
+        console.log(city);
+        console.log(data.main.temp);
+        console.log(data.weather[0].description);
+
+        // Удаляем предыдущую карточку, если она имеется
+        const prevCard = document.querySelector(".card");
+        if (prevCard) prevCard.remove();
+
+        // Создаем разметку для карточки
+        const html = `<div class = "card">
+
+            <h2 class="card-city">${city}</h2>
+            
+            <div class="card-weather">
+                <div class="card-value">${Math.round(data.main.temp)}<span>℃</span></div>
+                <img src="./img/Lightning.png" alt="Weather Conditions">
+            </div>
+
+            <div class="card-description">${data.weather[0].description}</div>        
+
+        </div>`
+
+        // Вставляем разметку карточки после заголовка
+        header.insertAdjacentHTML("afterend", html);
+
+    } catch(err) {
+        // Удаляем предыдущую карточку, если она имеется
+        const prevCard = document.querySelector(".card");
+        if (prevCard) prevCard.remove();
+
+        const errorMessage = "Город не найден";
+        const html = `<div class="card">${errorMessage}</div>`;
+
+        // Вставляем разметку карточки после заголовка
+        header.insertAdjacentHTML("afterend", html);
     }
 }
